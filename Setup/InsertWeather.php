49ce7e05c09eccd2719cc56209a119a75d738d93
sam_chong <sam_chong@mail.chungyo.net>
2020-08-27 17:48:44 +0800

    arrange file =D

---
+++ Setup/InsertWeather.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('ConfigDb.php') ;
+
+require_once('OneDayHalf-Insert.php') ;
+
+require_once('TwoDay-Weather.php') ;
+
+require_once('Week-Weather.php') ;
+
+exit() ;
+?>
--- Setup/TwoDay-Insert.php
+++ Setup/TwoDay-Insert.php
@@ -23,7 +23,6 @@
 $json = file_get_contents('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-0387CE9B-6B9C-43AB-A6F7-E03CC452690C&elementName=Wx,T,PoP6h') ;
 $obj = json_decode($json) ;
 
-require_once('configdb.php') ;
 @$db = setupDb() ;
 
 $command = <<< sqlDoc
